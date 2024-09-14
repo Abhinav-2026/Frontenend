976b2bb21382d58d417b4df4ef7387a59b6d4013
Abhinav-2026 <iabhinavkumar1625@gmail.com>
2024-09-14 08:42:21 +0530

    OTP LOGIN

---
+++ OTPLogin/otp/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import PhoneOtpLogin from './components/PhoneOtpLogin';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Login With Phone</h1>
+      <PhoneOtpLogin/>
+    </div>
+  );
+}
+
+export default App;
